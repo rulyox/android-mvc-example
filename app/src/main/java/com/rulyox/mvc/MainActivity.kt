672aa5304e0ba7c750fe562a389f6bf5eb42357d
rulyox <email@rulyox.com>
2020-07-03 17:00:44 +0900

    Initial commit

---
+++ app/src/main/java/com/rulyox/mvc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rulyox.mvc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(main_toolbar)
+
+    }
+
+}
