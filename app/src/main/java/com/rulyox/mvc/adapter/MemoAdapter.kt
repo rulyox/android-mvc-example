066361480fd96f310bdc834f20144623e86f50cf
rulyox <email@rulyox.com>
2020-07-05 10:54:01 +0900

    Implemented delete memo

--- app/src/main/java/com/rulyox/mvc/adapter/MemoAdapter.kt
+++ app/src/main/java/com/rulyox/mvc/adapter/MemoAdapter.kt
@@ -1,12 +1,16 @@
 package com.rulyox.mvc.adapter
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.rulyox.mvc.R
+import com.rulyox.mvc.activity.MainActivity
 import com.rulyox.mvc.memo.Memo
+import com.rulyox.mvc.memo.MemoStore
 
 class MemoAdapter: RecyclerView.Adapter<MemoAdapter.MemoViewHolder>() {
 
@@ -20,16 +24,44 @@ class MemoAdapter: RecyclerView.Adapter<MemoAdapter.MemoViewHolder>() {
         return memoList?.size ?: 0
     }
 
-    class MemoViewHolder(view: View, adapter: MemoAdapter): RecyclerView.ViewHolder(view) {
-        var title: TextView = view.findViewById(R.id.item_title)
-        var text: TextView = view.findViewById(R.id.item_text)
+    class MemoViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+        private val parent: LinearLayout = view.findViewById(R.id.item_parent)
+        val title: TextView = view.findViewById(R.id.item_title)
+        val text: TextView = view.findViewById(R.id.item_text)
+
+        init {
+
+            parent.setOnClickListener {
+
+                AlertDialog.Builder(view.context)
+                    .setTitle(R.string.dialog_delete_memo)
+                    .setMessage(R.string.dialog_delete_text)
+                    .setPositiveButton(R.string.dialog_delete) { dialog, _ ->
+
+                        MemoStore.delete(adapterPosition)
+                        MainActivity.updateAdapter()
+
+                        dialog.dismiss()
+
+                    }
+                    .setNegativeButton(R.string.dialog_cancel) { dialog, _ ->
+
+                        dialog.dismiss()
+
+                    }
+                    .show()
+
+            }
+
+        }
+
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): MemoViewHolder {
 
         val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.item, viewGroup, false)
-
-        return MemoViewHolder(view, this)
+        return MemoViewHolder(view)
 
     }
 
